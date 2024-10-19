e77b31cf6b59e392742b4a8ea8cc7bb8db85a6a6
AhmadTigress <139285648+AhmadTigress@users.noreply.github.com>
2024-10-19 06:56:29 +0100

    Add files via upload

---
+++ Python_variable.py
@@ -0,0 +1,18 @@
+name = input("what is your name?")
+length = len(name)
+
+print(name)
+print(length)
+
+#name = "Ahmad Rufai"
+length = len(name)
+
+print(name)
+print(length)
+
+#adding new names
+a = "Ahmad"
+b = "Rufai"
+print(a + b)
+print(a + b + name)
+print(a + " " + b + " " + name)
